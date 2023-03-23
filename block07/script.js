fb3b24309f1f11b8f31c3b79ea257ac20b5357b2
Jesse persinger <70872264+Mau5trap7791@users.noreply.github.com>
2023-03-23 16:46:15 -0400

    did at work

---
+++ block07/script.js
@@ -0,0 +1,8 @@
+//var1 +  will generate the number 10
+a = 8 + 2;
+//var2 - will generate the number 40
+b = 88 - 48;
+//var3 / will generate the number 39
+c = 195 / 5;
+//alert to promt customer of code
+alert('You have received this message because you have been chosen to open an important vault. Here is the secret combination:' + a + "," + b + "," + c);
